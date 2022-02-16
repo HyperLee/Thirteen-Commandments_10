79fa007f1617903361abdb420d71623e1ef020b9
Peter.Chiu <tidub76217@hotmail.com>
2022-02-16 21:53:31 +0800

    Update main.c

--- Thirteen-Commandments_10/main.c
+++ Thirteen-Commandments_10/main.c
@@ -2,13 +2,24 @@
 //  main.c
 //  Thirteen-Commandments_10
 //
-//  Created by 邱字立 on 2022/2/16.
+//  Created by HyperLee on 2022/2/16.
 //
+//  Item 10 不要在 stack 設置過大的變數以避免堆疊溢位(stack overflow)
+//  https://www.ptt.cc/bbs/C_and_CPP/M.1465304337.A.9F2.html
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int i = 0;
+    //int array[10000000]; // 在stack宣告過大陣列, 改用下列方式宣告
+    int *array = (int*) malloc( 10000000*sizeof(int) );
+    
+    for(i = 0; i < 10000000; i++)
+    {
+        array[i] = i;
+        printf("array[%d] %d\n", i, array[i]);
+    }
+    
     return 0;
 }
